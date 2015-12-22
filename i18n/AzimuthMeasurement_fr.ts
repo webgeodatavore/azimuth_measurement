d459d9e6265715ddcf155b2582bba0b2b22da0e9
ThomasG77 <thomas_gratier@yahoo.fr>
2015-12-22 16:34:19 +0100

    Change to internationalize plugin with French translation

---
+++ i18n/AzimuthMeasurement_fr.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished">Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>AzimuthMeasurement</name>
+    <message>
+        <location filename="azimuth_measurement.py" line="109"/>
+        <source>Azimut Measurement</source>
+        <translation type="unfinished">Mesure d&apos;azimuth</translation>
+    </message>
+</context>
+<context>
+    <name>DockWidget</name>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="14"/>
+        <source>Measure azimuth</source>
+        <translation type="unfinished">Mesure d&apos;azimuth</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="30"/>
+        <source>Geographic distance</source>
+        <translation type="unfinished">Distance géographique</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="41"/>
+        <source>centimeters</source>
+        <translation type="unfinished">centimètres</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="46"/>
+        <source>meters</source>
+        <translation type="unfinished">mètres</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="51"/>
+        <source>kilometers</source>
+        <translation type="unfinished">kilomètres</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="59"/>
+        <source>Azimuth (degrees)</source>
+        <translation type="unfinished">Azimuth (en degrés)</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="69"/>
+        <source>Reverse Azimuth (degrees)</source>
+        <translation type="unfinished">Azimuth inversé (en degrés)</translation>
+    </message>
+    <message>
+        <location filename="azimuth_measurement_widget.ui" line="79"/>
+        <source>Clear</source>
+        <translation type="unfinished">Nettoyer</translation>
+    </message>
+</context>
+</TS>
